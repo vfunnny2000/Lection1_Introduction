4194e948f186132612ed52d196e20ff37dd923ef
vfunnny2000 <OVDuganova@yandex.ru>
2022-08-09 12:57:56 +0300

    added user greeting

---
+++ Examples/Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввести имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "olga")
+{
+    Console.WriteLine("Hello darling!");
+}
+else
+{
+    Console.WriteLine("Hi");
+    Console.WriteLine(username);
+}
